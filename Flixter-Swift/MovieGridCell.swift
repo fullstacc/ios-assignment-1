07582656195c133841dfca26ec78169da37f2e60
sulfuroussquares <bbetancova@gmail.com>
2022-03-05 08:00:58 -0700

    added collection view

---
+++ Flixter-Swift/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flixter-Swift
+//
+//  Created by BRIAN BETANCOURT on 3/4/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
--- Flixter-Swift/MoviesViewController.swift
+++ Flixter-Swift/MoviesViewController.swift
@@ -50,14 +50,17 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         // find selected movie
         let cell = sender as! UITableViewCell
-        let indexPath = tableView.indexPath(for: <#T##UITableViewCell#>)!
+        let indexPath = tableView.indexPath(for: cell)!
         let movie = movies[indexPath.row]
         // pass selected movie to detailsviewcontroller
         let detailsViewController = segue.destination as! MovieDetailsViewController
         
         detailsViewController.movie = movie
         
-        tableView.deselectRow(at: indexPath, animated: true)
+        tableView.deselectRow(at: indexPath, animated: false)
+        
+        
+        
         
     }
     
